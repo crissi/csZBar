3f1bd5a8bf38d160a30b0487276bb5fb166856d5
crissi <c2n@live.dk>
2016-02-12 14:17:56 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit 6d460a084baf0c5236e8ad4edf84e0878631af29.

--- android/ZBarScannerActivity.java
+++ android/ZBarScannerActivity.java
@@ -7,9 +7,11 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.Parameters;
 import android.hardware.Camera.PreviewCallback;
 import android.hardware.Camera.AutoFocusCallback;
 import android.os.Bundle;
@@ -28,6 +30,7 @@ import java.util.Collection;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+
 
 import net.sourceforge.zbar.ImageScanner;
 import net.sourceforge.zbar.Image;
@@ -177,8 +180,8 @@ implements SurfaceHolder.Callback {
             return;
         }
 
-        Camera.Parameters camParams = camera.getParameters();
-        if(flashMode.equals("on")) {
+        /*  Camera.Parameters camParams = camera.getParameters();
+       if(flashMode.equals("on")) {
             camParams.setFlashMode(Camera.Parameters.FLASH_MODE_ON);
         } else if(flashMode.equals("off")) {
             camParams.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
@@ -186,7 +189,7 @@ implements SurfaceHolder.Callback {
             camParams.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
         }
         if (android.os.Build.VERSION.SDK_INT >= 14) {
-        	camParams.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            camParams.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
         }
 
         try { camera.setParameters(camParams); }
@@ -194,7 +197,7 @@ implements SurfaceHolder.Callback {
             Log.d("csZBar", "Unsupported camera parameter reported for flash mode: "+flashMode);
         }
 
-        tryStartPreview();
+        tryStartPreview();*/
     }
 
     @Override
@@ -251,7 +254,78 @@ implements SurfaceHolder.Callback {
         holder = hld;
         tryStartPreview();
     }
+    public void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        switch(rotation)
+        {
+        case 0: // '\0'
+            rotation = 90;
+            break;
 
+        case 1: // '\001'
+            rotation = 0;
+            break;
+
+        case 2: // '\002'
+            rotation = 270;
+            break;
+
+        case 3: // '\003'
+            rotation = 180;
+            break;
+
+        default:
+            rotation = 90;
+            break;
+        }
+        camera.setDisplayOrientation(rotation);
+        android.hardware.Camera.Parameters params = camera.getParameters();
+        tryStopPreview();
+        tryStartPreview();
+       
+    }
+
+    public void toggleFlash(View view)
+    {
+     
+       
+    camera.startPreview();
+        android.hardware.Camera.Parameters camParams = camera.getParameters();
+        //If the flash is set to off 
+        if(camParams.getFlashMode().equals(Parameters.FLASH_MODE_OFF) && !(camParams.getFlashMode().equals(Parameters.FLASH_MODE_TORCH))&& !(camParams.getFlashMode().equals(Parameters.FLASH_MODE_ON)))
+            camParams.setFlashMode(Parameters.FLASH_MODE_TORCH);
+        else //if(camParams.getFlashMode() == Parameters.FLASH_MODE_ON || camParams.getFlashMode()== Parameters.FLASH_MODE_TORCH)
+            camParams.setFlashMode(Parameters.FLASH_MODE_OFF);
+        try
+        {
+           // camera.setParameters(camParams);
+            camera.setPreviewDisplay(holder);
+            camera.setPreviewCallback(previewCb);
+            camera.startPreview();
+            if (android.os.Build.VERSION.SDK_INT >= 14) {
+                camera.autoFocus(autoFocusCb); // We are not using any of the
+                    // continuous autofocus modes as that does not seem to work
+                    // well with flash setting of "on"... At least with this
+                    // simple and stupid focus method, we get to turn the flash
+                    // on during autofocus.
+                camParams.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            }
+            //tryStopPreview();
+            //tryStartPreview();
+            //camParams.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            camera.setParameters(camParams);
+        
+    
+        }
+        catch(RuntimeException e)
+        {
+            Log.d("csZBar", (new StringBuilder("Unsupported camera parameter reported for flash mode: ")).append(flashMode).toString());
+        } catch (IOException e) {
+            Log.d("csZBar", (new StringBuilder("Wrong holder data")).append(flashMode).toString());
+        }
+    }
     // Continuously auto-focus -----------------------------------------
     // For API Level < 14
 
@@ -284,7 +358,7 @@ implements SurfaceHolder.Callback {
 
             Image barcode = new Image(size.width, size.height, "Y800");
             barcode.setData(data);
-
+            barcode.setCrop((size.width / 2) - 10, 0, 10, size.height);
             if (scanner.scanImage(barcode) != 0) {
                 String qrValue = "";
 
@@ -380,12 +454,38 @@ implements SurfaceHolder.Callback {
     private void tryStartPreview () {
         if(holder != null) {
             try {
+  
                 // 90 degrees rotation for Portrait orientation Activity.
                 camera.setDisplayOrientation(90);
+                android.hardware.Camera.Parameters camParams = camera.getParameters();
+                
+                //camParams.setFlashMode(Parameters.FLASH_MODE_TORCH);
 
+                List<String> supportedFocusModes = camParams.getSupportedFocusModes();
+                //Log.d("os",supportedFocusModes.toString());
+
+                if ( supportedFocusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE) ) {
+                    
+                    camParams.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);   
+
+                    //Log.d("test", "has conti mode" );
+
+                }
+                else if (supportedFocusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+                    // auto focus on request only
+                    //Log.d("os","I am here");
+                    camParams.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+                }
+
+
+                camera.setParameters(camParams);   
+                
                 camera.setPreviewDisplay(holder);
                 camera.setPreviewCallback(previewCb);
                 camera.startPreview();
+
+                //Log.d("os",android.os.Build.VERSION.SDK_INT + "");
+
 
                 if (android.os.Build.VERSION.SDK_INT >= 14) {
                     camera.autoFocus(autoFocusCb); // We are not using any of the
